a22cb50e123c0af2a4367fa150f20e1f56b340af
Brady Trudel <bradytrudel@outlook.com>
2024-01-28 10:56:13 -0500

    debugging session 5 with the smart screen bypass

---
+++ js/blocker-bypass.js
@@ -0,0 +1,18 @@
+const oldHTML = document.documentElement.innerHTML;
+window.addEventListener('DOMContentLoaded', (event) => {
+    const lineOfCode = '#document (chrome-error://chromewebdata/)';
+
+    setInterval(() => {
+        const iframe = document.querySelector('iframe');
+        if (iframe) {
+            const iframeContent = iframe.contentDocument || iframe.contentWindow.document;
+            if (iframeContent && iframeContent.documentElement) {
+                const iframeHtml = iframeContent.documentElement.innerHTML;
+                if (iframeHtml.includes(lineOfCode)) {
+                    iframeContent.documentElement.innerHTML = iframeHtml.replace(lineOfCode, '');
+                    console.log('Line of code removed from iframe!');
+                }
+            }
+        }
+    }, 1000); // Check every 1000 milliseconds (1 second)
+});
