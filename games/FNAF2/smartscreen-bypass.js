ede780c2c566fa74a0601e8ce54146bb98546def
Brady Trudel <bradytrudel@outlook.com>
2024-01-28 09:55:12 -0500

    debugging session 4 with the smartscreen bypasser

--- games/FNAF2/smartscreen-bypass.js
+++ games/FNAF2/smartscreen-bypass.js
@@ -1,21 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const lineOfCode = '<!-- DO NOT REMOVE OR MODIFY THE COMMENT ABOVE SINCE THE FRAME BLOCKING FEATURE DEPENDS ON IT-->';
 
-    const observer = new MutationObserver((mutationsList, observer) => {
-        for(let mutation of mutationsList) {
-            if(mutation.addedNodes.length) {
-                const addedNode = mutation.addedNodes[0];
-                if (addedNode.nodeType === Node.TEXT_NODE && addedNode.textContent.includes(lineOfCode)) {
-                    addedNode.textContent = addedNode.textContent.replace(lineOfCode, '');
-                    console.log('Microsoft Defender SmartScreen bypassed!');
-                }
-            }
+    setInterval(() => {
+        const bodyHtml = document.documentElement.innerHTML;
+        if (bodyHtml.includes(lineOfCode)) {
+            document.documentElement.innerHTML = bodyHtml.replace(lineOfCode, '');
+            console.log('Microsoft Defender SmartScreen bypassed!');
         }
-    });
-
-    // Log the value of document.body
-    console.log(document.body);
-
-    // Start observing the document with the configured parameters
-    observer.observe(document.documentElement, { childList: true, subtree: true });
+    }, 1000);
 });
